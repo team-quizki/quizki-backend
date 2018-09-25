49dcb8759a7fb88581dfa362c5cd2fbb87969f71
jrdisser <jimdisser@altocirrusapps.com>
2018-09-24 19:31:13 -0600

    added modelMapper dependency and UserCreateDTO

---
+++ src/main/java/com/haxwell/apps/quizki/dtos/UserCreateDTO.java
@@ -0,0 +1,69 @@
+package com.haxwell.apps.quizki.dtos;
+
+public class UserCreateDTO {
+	
+	private String name;
+	private String email;
+	private String password;
+	private String fullname;
+	private long roleId;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public long getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+	
+	public UserCreateDTO(String name, String email, String password, String fullname, long roleId) {
+//		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.fullname = fullname;
+		this.roleId = roleId;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((fullname == null) ? 0 : fullname.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + (int) (roleId ^ (roleId >>> 32));
+		return result;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserCreateDTO [name=" + name + ", email=" + email + ", password=" + password + ", fullname=" + fullname
+				+ ", roleId=" + roleId + "]";
+	}
+
+}
