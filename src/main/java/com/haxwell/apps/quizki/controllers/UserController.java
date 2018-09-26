a8377a7130927915b8bb7a1aeb60f3dfba87e019
jrdisser <jimdisser@altocirrusapps.com>
2018-09-26 16:44:53 -0600

    added mapping to UserServiceImpl and supporting updates to UserController, User, CreatedUserDTO

--- src/main/java/com/haxwell/apps/quizki/controllers/UserController.java
+++ src/main/java/com/haxwell/apps/quizki/controllers/UserController.java
@@ -1,35 +1,20 @@
 package com.haxwell.apps.quizki.controllers;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-
 import com.haxwell.apps.quizki.dtos.CreatedUserDTO;
 import com.haxwell.apps.quizki.dtos.UserCreateDTO;
-import com.haxwell.apps.quizki.entities.User;
-import com.haxwell.apps.quizki.entities.UserRole;
-import com.haxwell.apps.quizki.exceptions.UserFieldsNotUniqueException;
-import com.haxwell.apps.quizki.exceptions.UserRoleNotInDatabaseException;
-import com.haxwell.apps.quizki.exceptions.ValidationErrorData;
-import com.haxwell.apps.quizki.repositories.UserRepository;
-import com.haxwell.apps.quizki.repositories.UserRoleRepository;
 import com.haxwell.apps.quizki.services.UserService;
 
 @CrossOrigin (origins = "http://localhost:4200")
@@ -58,11 +43,8 @@ public class UserController {
 		
 		fields = uService.emailOrNameIsUnique(fields);
 		
-		
 		return new ResponseEntity<HashMap<String,String>>(fields, HttpStatus.OK) ;
 				
 	}
-		
-	
 
 }
