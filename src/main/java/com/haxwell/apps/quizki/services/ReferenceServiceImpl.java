46f1d549514e573f6f3b511715bb0ae701384d16
Johnathan James <haxwell@gmail.com>
2018-10-07 13:23:47 -0600

    Added @Service annotation to ReferenceService

--- src/main/java/com/haxwell/apps/quizki/services/ReferenceServiceImpl.java
+++ src/main/java/com/haxwell/apps/quizki/services/ReferenceServiceImpl.java
@@ -3,12 +3,14 @@ package com.haxwell.apps.quizki.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.haxwell.apps.quizki.entities.Reference;
 import com.haxwell.apps.quizki.repositories.ReferenceRepository;
 
 import net.minidev.json.JSONArray;
 
+@Service
 public class ReferenceServiceImpl implements ReferenceService {
 
 	@Autowired
