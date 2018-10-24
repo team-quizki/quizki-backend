5bfd02ec85bd674e73a98ddf158e31739aebafad
jrdisser <jimdisser@altocirrusapps.com>
2018-10-23 19:42:28 -0600

    removed pagination from QuestionRepository

--- src/main/java/com/haxwell/apps/quizki/repositories/QuestionRepository.java
+++ src/main/java/com/haxwell/apps/quizki/repositories/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.haxwell.apps.quizki.repositories;
 
 import java.awt.print.Pageable;
+import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Repository;
 
 import com.haxwell.apps.quizki.entities.Question;
 
+//TODO: implement pagination here and in the service layer
+
 @Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
-	Page<Question> findByUserId(long userId, Pageable pageable);
+	Set<Question> findByUserId(long userId);
 
 }
