2ea118a292d212cda2fa94ef4ab7eadde956d73b
Johnathan James <haxwell@gmail.com>
2019-02-04 22:11:25 -0700

    Populate DB Script, set the type of the is_correct field on Choice to tinyint(1), or boolean. It was previously set to bit.

--- src/main/resources/META-INF/sql/populate_quizki2_db_20190201.sql
+++ src/main/resources/META-INF/sql/populate_quizki2_db_20190201.sql
@@ -34,7 +34,7 @@ CREATE TABLE `choice` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `text` varchar(10000) NOT NULL,
   `sequence` bigint(20) NOT NULL,
-  `is_correct` bit(1) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL,
   `question_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_question_choice_idx` (`question_id`),
